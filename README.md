2bc5cee2e40e72ec041d0bd27162c419722c5392
John Kleber <jo.kleber85@gmail.com>
2021-11-07 01:15:29 -0300

    Curso concluido

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Alura
 Formação Flutter</br>
 
 Persistência com Flutter: Crie um app com armazenamento interno (08 horas)</br>
+
+PS.: Lembrando que para adicionar uma imagem deve se alterar o arquivo pubspec.yaml -> assets -> pubget
+
+Variados tipos de personalização: [CookBook](https://flutter.dev/docs/cookbook)</br>
---
+++ bytebank2/lib/screens/contacts_list.dart
@@ -0,0 +1,81 @@
+import 'package:bytebank2/database/app_database.dart';
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatefulWidget {
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  // final ContactDao _dao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: [],
+        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('Loading...'),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact>? contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts![index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts?.length,
+              );
+          }
+          return Text('Unknown error');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => ContactForm()))
+              .then((value) => setState(() {}));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(contact.name, style: TextStyle(fontSize: 24.0)),
+        subtitle: Text(contact.accountNumber.toString(),
+            style: TextStyle(fontSize: 16.0)),
+      ),
+    );
+  }
+}
